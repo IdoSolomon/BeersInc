f7db4c57ee5fd1a422378f845de727d4f7ef1ba7
IdoSolomon <idosol@gmail.com>
2017-06-25 15:37:56 +0300

    Almost working ForgotPassword

--- public/app/components/controllers/loginController.js
+++ public/app/components/controllers/loginController.js
@@ -2,12 +2,12 @@
  * Created by Liorpe on 24/06/2017.
  */
 angular.module('myApp')
-    .controller('loginController', ['UserService', '$http', "$location", "$window",
-        function(UserService, $http, $location, $window){
-            var self = this;
+    .controller('loginController', ['UserService', '$http', "$location", "$window", "$scope",
+        function(UserService, $http, $location, $window, $scope){
+            let self = this;
             self.msg = 'Login';
             self.user = {"username": '', "password": ''};
-
+            self.showFP = false;
             self.login = function(valid) {
                 if (valid) {
                     UserService.login(self.user).then(function (success) {
@@ -19,5 +19,10 @@ angular.module('myApp')
                     })
                 }
 
+            };
+            $scope.toggleFP = function() {
+                if(self.showFP === true)
+                    self.showFP = false;
+                else self.showFP = true;
             };
     }]);
